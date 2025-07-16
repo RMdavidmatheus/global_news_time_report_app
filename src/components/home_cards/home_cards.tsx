bd1080bb62e34275c2069188c1f5c6c6150b9ed9
David Alejandro Mateus Martinez <davidalejandro.mateus.contractor@bbva.com>
2025-07-16 17:56:29 -0500

    Agregar nuevas imágenes y actualizar el contenido de los componentes de tarjetas en la página de inicio para reflejar mejor las funcionalidades de la aplicación.

--- src/components/home_cards/home_cards.tsx
+++ src/components/home_cards/home_cards.tsx
@@ -2,75 +2,75 @@ import { Card, CardFooter, Image } from "@heroui/react";
 
 export default function HomeCardsComponent() {
   return (
-    <div className="grid grid-cols-4 gap-20 w-full">
+    <div className="grid grid-cols-4 gap-15 w-full">
       <Card isFooterBlurred className="border-none" radius="lg">
         <Image
-          alt="Woman listing to music"
+          alt="Activities"
           className="object-cover"
           height={400}
           src="/images/1.png"
           width={400}
         />
-        <CardFooter className="flex flex-col justify-center bg-black/60 before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10 h-[45px] text-center">
+        <CardFooter className="flex flex-col justify-center bg-black/60 before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10 h-[80px] text-center">
           <p className="text-md font-bold text-white/80">
             Control total de tus actividades
           </p>
           <p className="text-md text-white/80">
-            Crea, edita y elimina actividades
+            Crea, edita y elimina tareas diarias que llegan a tu correo
           </p>
         </CardFooter>
       </Card>
 
       <Card isFooterBlurred className="border-none" radius="lg">
         <Image
-          alt="Woman listing to music"
+          alt="Time and efficiency"
           className="object-cover"
           height={400}
-          src="/images/1.png"
+          src="/images/2.png"
           width={400}
         />
-        <CardFooter className="flex flex-col justify-center bg-black/60 before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10 h-[45px] text-center">
+        <CardFooter className="flex flex-col justify-center bg-black/60 before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10 h-[80px] text-center">
           <p className="text-md font-bold text-white/80">
-            Control total de tus actividades
+            Eficiencia y productividad
           </p>
           <p className="text-md text-white/80">
-            Crea, edita y elimina actividades
+            Mejora tu tiempo y eficiencia disminuyendo los tiempos muertos
           </p>
         </CardFooter>
       </Card>
 
       <Card isFooterBlurred className="border-none" radius="lg">
         <Image
-          alt="Woman listing to music"
+          alt="Intuitive interface"
           className="object-cover"
           height={400}
-          src="/images/1.png"
+          src="/images/3.png"
           width={400}
         />
-        <CardFooter className="flex flex-col justify-center bg-black/60 before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10 h-[45px] text-center">
+        <CardFooter className="flex flex-col justify-center bg-black/60 before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10 h-[80px] text-center">
           <p className="text-md font-bold text-white/80">
-            Control total de tus actividades
+            Interfaz intuitiva
           </p>
           <p className="text-md text-white/80">
-            Crea, edita y elimina actividades
+            Fácil de usar y entender, no necesitas ser un experto en tecnología para usarlo
           </p>
         </CardFooter>
       </Card>
 
       <Card isFooterBlurred className="border-none" radius="lg">
         <Image
-          alt="Woman listing to music"
+          alt="Security"
           className="object-cover"
           height={400}
-          src="/images/1.png"
+          src="/images/4.png"
           width={400}
         />
-        <CardFooter className="flex flex-col justify-center bg-black/60 before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10 h-[45px] text-center">
+        <CardFooter className="flex flex-col justify-center bg-black/60 before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10 h-[80px] text-center">
           <p className="text-md font-bold text-white/80">
-            Control total de tus actividades
+            Seguridad
           </p>
           <p className="text-md text-white/80">
-            Crea, edita y elimina actividades
+            Tu información está protegida y no se comparte con terceros
           </p>
         </CardFooter>
       </Card>
